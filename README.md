1c4ff8ab9fa1b44f170082ee373db7e389ec4c33
Mohammad Shahbaz Alam <shahbaz@magic.link>
2021-06-12 13:33:05 +0530

    Magic - DID Token

---
+++ README.md
@@ -0,0 +1,28 @@
+# Magic - Get a DID Token
+
+[![Deploy with Vercel](https://vercel.com/button)](https://vercel.com/new/git/external?repository-url=https%3A%2F%2Fgithub.com%2Fshahbaz17%2Fmagic-didtoken&env=NUXT_ENV_MAGIC_PUBLISHABLE_KEY,MAGIC_SECRET_KEY) [![Deploy with Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/shahbaz17/magic-didtoken)
+
+# Quick Start Instructions
+
+```bash
+$ git clone https://github.com/shahbaz17/magic-didtoken
+$ cd magic-didtoken
+$ cp .env.example .env
+# enter your Magic API keys in your .env file
+$ yarn install
+$ yarn dev
+# starts app at http://localhost:3000
+```
+
+## Environment Variables
+
+```
+NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY=pk_live_
+MAGIC_SECRET_KEY=sk_live_
+```
+
+## Get your Magic Keys
+
+Try Magic for free [here](https://magic.link/invite/r/kpD9rMJFZGPqVvx1) and get your `NEXT_PUBLIC_MAGIC_PUBLISHABLE_KEY` and `MAGIC_SECRET_KEY`
+
+![Magic Dashboard](https://dev-to-uploads.s3.amazonaws.com/uploads/articles/yiehdmd2kpokpeom3rqh.png)
---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import React, { useEffect, useState, useCallback } from 'react'
+import Router from 'next/router'
+import { magic } from '../magic'
+import Loading from '../components/loading'
+
+export default function Index() {
+	const [userMetadata, setUserMetadata] = useState()
+	const [didToken, setDidToken] = useState()
+
+	useEffect(() => {
+		// On mount, we check if a user is logged in.
+		// If so, we'll retrieve the authenticated user's profile.
+		magic.user.isLoggedIn().then((magicIsLoggedIn) => {
+			if (magicIsLoggedIn) {
+				magic.user.getMetadata().then(setUserMetadata)
+				magic.user.getIdToken({ lifespan: 60 * 60 * 10 }).then(setDidToken)
+			} else {
+				// If no user is logged in, redirect to `/login`
+				Router.push('/login')
+			}
+		})
+	}, [])
+
+	/**
+	 * Perform logout action via Magic.
+	 */
+	const logout = useCallback(() => {
+		magic.user.logout().then(() => {
+			Router.push('/login')
+		})
+	}, [Router])
+
+	return userMetadata && didToken ? (
+		<div className="container">
+			<h1>Current user: {userMetadata.email}</h1>
+			<strong>✨DID Token✨</strong>
+			<pre>{didToken}</pre>
+			<p>
+				Validity of the DID Token is <strong>10 hours.</strong> Read more{' '}
+				<a
+					href="https://docs.magic.link/client-sdk/web/api-reference#getidtoken"
+					target="_blank"
+				>
+					here
+				</a>
+				.
+			</p>
+			<button onClick={logout}>Logout</button>
+		</div>
+	) : (
+		<Loading />
+	)
+}
